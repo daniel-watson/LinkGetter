7889bf9ebf19c28238a59984079c83ce44cb4b7e
Daniel Watson <dan.watson101@gmail.com>
2014-01-22 17:45:23 +0000

    Can get each URL for each run detail page on the first page of results

--- src/linkgetter/LinkGetter.java
+++ src/linkgetter/LinkGetter.java
@@ -16,27 +16,38 @@ import org.jsoup.select.Elements;
 public class LinkGetter {
  
   public static void main(String[] args) {
- 
-	Document doc;
-	try {
- 
-		// need http protocol
-		doc = Jsoup.connect("http://www.runnersworld.co.uk/events/foundevents.asp?v=2&evntTitle=&evntDate=a%3afut&distance=&county=&area=&distanceFromHome=&evntSurface=&evntGround=&evntWheelChairAccessible=").get();
+ 	try {
+                String url = "http://www.runnersworld.co.uk/events/foundevents.asp?v=2&evntTitle=&evntDate=a%3afut&distance=&county=&area=&distanceFromHome=&evntSurface=&evntGround=&evntWheelChairAccessible=";
+		//use Jsoup connect to point it to the correct page 
+                //use .get or the get HTTP protocol toreturn the web page.
+		Document doc = Jsoup.connect(url)
+                        .userAgent("Mozilla")
+                        .timeout(3000)
+                        .get();
  
 		// get page title
 		String title = doc.title();
+                
+                //Finds matching elements. it will look in for a class named evntTitle.
+                //It will then save any <ahref> links to the list links
+                Elements links = doc.select(".evntTitle > a[href]");
+                System.out.println(links.size());
+
+                //print the title of the web page 
 		System.out.println("title : " + title);
  
-		// get all links
-		Elements links = doc.select("a[href]");
+                //counter ensure i only recieve 10 links
+                int i = 1;
 		for (Element link : links) {
- 
 			// get the value from href attribute
-			System.out.println("\nlink : " + link.attr("href"));
-			System.out.println("text : " + link.text());
- 
+			System.out.println("\nLink : " + "http://www.runnersworld.co.uk" + link.attr("href"));
+                        //int count = 0;
+                        //while(count < 100000000){count++;}
+			System.out.println("Race Title : " + link.text());
+                        System.out.println(i++);
+                        //count = 0;
 		}
- 
+
 	} catch (IOException e) {
 		e.printStackTrace();
 	}
