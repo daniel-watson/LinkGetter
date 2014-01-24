5422b4b498aeff261232d0e1fccac7a831bb0167
Daniel Watson <dan.watson101@gmail.com>
2014-01-24 20:44:34 +0000

    Added extra classes for better design. Yet to Implement.

---
+++ src/linkgetter/ResultPageLink.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkgetter;
+
+/**
+ *
+ * @author danielwatson
+ */
+public class ResultPageLink {
+     private String pageLink;
+    
+    public ResultPageLink(String pageLink){
+        this.pageLink = pageLink;
+    }
+}
